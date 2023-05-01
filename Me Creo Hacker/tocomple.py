827db725993e758f876a1dc562180916ab4bbd44
jc <jorge.cumminz@gmail.com>
2023-04-30 20:08:47 -0400

    Update tocomple.py

--- Me Creo Hacker/tocomple.py
+++ Me Creo Hacker/tocomple.py
@@ -33,3 +33,58 @@ def erroropera(noerroa, noerrob, erro1, erro2, operacion):
     else:
         return 'soi weon o te haci?'
     
+
+
+# Funcion que saca la media de una lista de datos
+# Recibe una lista, cuenta la cantidad de datos, suma los datos y los divide
+# Debe haber otra, pero preferi irme a la segura 
+# "datos" es la lista de datos que se quiere obtener la media
+def media(datos):
+    cantDatos = len(datos)
+    sumaDatos = 0
+    i = 0
+    while i < cantDatos:
+        if i == cantDatos:
+            break
+        else:
+            sumat = sumaDatos + datos[i]
+            sumaDatos = sumat
+            i = i+1
+        media = sumaDatos/cantDatos
+    return media    
+
+
+
+# Funcion que calcula la desviacon estandar para cada una lista de valores. 
+# Dependiendo de que se este estudiando, se debe seleccionar de que tipo de 
+# datos se habla. 
+# "datos" es la lista de datos a operar
+# "purgador" es el tipo de muestra que se esta tratando. estas serian
+# 0, si es poblacion (T O D O  EL ESPACIO) y 1, si es una muestra del espacio
+def desvEstandar(datos, purgador):
+    promedio = media(datos)
+    cantDatos = len(datos) 
+    suma = 0
+    i = 0
+    while i<cantDatos:
+        if i == cantDatos:
+            break 
+        else: 
+            operatoria = (datos[i]-promedio)**2
+            sumat = suma + operatoria
+            suma = sumat
+            i = i+1
+    if purgador == 0:
+        total = cantDatos
+        megaPromedio = sumat/(total)
+        desviacion = np.sqrt(megaPromedio)
+    elif purgador == 1:
+        total = cantDatos - 1
+        megaPromedio = sumat/(total)
+        desviacion = np.sqrt(megaPromedio)
+    else: 
+        desviacion = 'agilao'
+    return desviacion 
+
+notas = [4.5, 5.8, 6.7]
+print(desvEstandar(notas, 0))
