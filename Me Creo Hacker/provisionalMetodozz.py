a96bb2268bd571356ddbff447b8bb3c4f063b959
Jorge Cummins <jorge.cumminz@gmail.com>
2023-05-09 02:48:28 -0400

    Update provisionalMetodozz.py

--- Me Creo Hacker/provisionalMetodozz.py
+++ Me Creo Hacker/provisionalMetodozz.py
@@ -17,7 +17,52 @@ def capacitancia(carga, diferenciaPoto):
     opa = [C, '[F] "Faradias"' ]
     return opa
 
-print(capacitancia('100', '2'))
+#x = input('Ingrese Capacitancia ')
+#d = input ('Ingrese Diferencia de potencial ')
+#motodozzzzzzz = capacitancia(x, d)
+#print(motodozzzzzzz)
 
 
 
+# Tipo es si serie paralelo o que wea sea..... 
+# 0 si es en serie, 1 si es en paralelo
+def capacitanciaEq(totalcapac, tipox, i=0):
+    tipo = int(float(tipox)//1)
+    if tipo == 0: 
+        largo = len(totalcapac)
+        sixsixsixthenumberofthebeast = []
+        while i < largo:
+            if i == largo: 
+                break
+            else: 
+                valor = float(totalcapac[i])
+                valorMatraca = 1/valor
+                runtothehills = sixsixsixthenumberofthebeast + [valorMatraca]
+                sixsixsixthenumberofthebeast = runtothehills
+                i = i+1
+            cs = sixsixsixthenumberofthebeast
+    elif tipo == 1: 
+        largo = len(totalcapac)
+        ironMan = []
+        while i < largo:
+            if i == largo: 
+                break
+            else: 
+                valor = float(totalcapac[i])
+                nib = ironMan + [valor] 
+                ironMan = nib
+                i = i+1
+            cs = ironMan
+    n = largo-1
+    damomentumXD = 0
+    while i < n:
+        if i == n:
+            break
+        else: 
+            lacochina = damomentumXD + float(cs[i])
+            damomentumXD = lacochina
+            i = i+1
+        sex = damomentumXD
+        print(sex)
+
+print(capacitanciaEq([2, 44, 3, 5, 3], 0))
