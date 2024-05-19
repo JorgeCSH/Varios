60dfab2b1ec033419f8664556cee46fffa1fc514
JorgeCSH <jorge.cumminz@gmail.com>
2024-05-18 21:13:24 -0400

    tgg

---
+++ Programas que les puse asi porque suena chevere/Codigos general/Prueba.py
@@ -0,0 +1,15 @@
+
+
+# Ahora algo mas profesional
+def nice(i=1):
+    numero = int(input('Inserte Numero '))
+    while not numero == 69:
+        return nice(i=i+1)
+    else:
+        if i == 1:
+            print('Te demoraste 1 intentos en darte cuenta de esto, ¡Felicitaciones!... nice')
+        else:
+            print('Te demoraste ' + str(i) + ' intentos en darte cuenta de esto, nice')
+
+
+
